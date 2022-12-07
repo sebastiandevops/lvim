f3bcc3fc76b779f60a7b7a4496acc68102897339
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-12-06 22:01:13 -0500

    Enable flake8

--- config.lua
+++ config.lua
@@ -261,7 +261,7 @@ require("lvim.lsp.manager").setup("pylsp", opts)
 -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  -- { command = "flake8", filetypes = { "python" } },
+  { command = "flake8", filetypes = { "python" } },
   {
     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
     command = "shellcheck",
