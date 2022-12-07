4d46f35728da756eb4a12712a742285b698777d8
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-12-07 11:13:40 -0500

    New pluging: code runner - jaq-nvim

--- config.lua
+++ config.lua
@@ -532,6 +532,7 @@ lvim.plugins = {
   },
   -- Neovim plugin to inserts a shebang line when editing a new file.
   { 'samirettali/shebang.nvim' },
+  -- Neovim plugin to open documentation in bottom window
   {
     "amrbashir/nvim-docs-view",
     opt = true,
@@ -542,6 +543,68 @@ lvim.plugins = {
         hight = 10,
       }
     end,
+  },
+  -- Code runner
+  {
+    "is0n/jaq-nvim",
+    config = function ()
+      require('jaq-nvim').setup{
+      cmds = {
+        -- Uses vim commands
+        internal = {
+          lua = "luafile %",
+          vim = "source %"
+        },
+        -- Uses shell commands
+        external = {
+          markdown = "glow %",
+          python   = "python3 %",
+          go       = "go run %",
+          sh       = "sh %"
+        }
+      },
+      behavior = {
+        -- Default type
+        default     = "float",
+        -- Start in insert mode
+        startinsert = false,
+        -- Use `wincmd p` on startup
+        wincmd      = false,
+        -- Auto-save files
+        autosave    = false
+      },
+      ui = {
+        float = {
+          -- See ':h nvim_open_win'
+          border    = "rounded",
+          -- See ':h winhl'
+          winhl     = "Normal",
+          borderhl  = "FloatBorder",
+          -- See ':h winblend'
+          winblend  = 0,
+          -- Num from `0-1` for measurements
+          height    = 0.8,
+          width     = 0.8,
+          x         = 0.5,
+          y         = 0.5
+        },
+        terminal = {
+          -- Window position
+          position = "bot",
+          -- Window size
+          size     = 10,
+          -- Disable line numbers
+          line_no  = false
+        },
+        quickfix = {
+          -- Window position
+          position = "bot",
+          -- Window size
+          size     = 10
+        }
+      }
+    }
+    end
   },
 }
 -- }}}1
