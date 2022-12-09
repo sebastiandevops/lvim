0ccfae7f8742ce6c505b789c67051493f0e8643d
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-12-08 20:49:50 -0500

    Update python3 path for dap configuration

--- config.lua
+++ config.lua
@@ -706,28 +706,28 @@ dap.configurations.python = dap.configurations.python or {}
     name = 'launch with options',
     program = '${file}';
     python = function() end,
-    pythonPath = function()
-        local path
-        for _, server in pairs(vim.lsp.buf_get_clients()) do
-            path = vim.tbl_get(server, 'config', 'settings', 'python', 'pythonPath')
-            if path then break end
-        end
-        path = vim.fn.input('Python path: ', path or '', 'file')
-        return path ~= '' and vim.fn.expand(path) or nil
-    end,
-    args = function()
-        local args = {}
-        local i = 1
-        while true do
-            local arg = vim.fn.input("Argument [" .. i .. "]: ")
-            if arg == '' then
-                break
-            end
-            args[i] = arg
-            i = i + 1
-        end
-        return args
-    end,
+    pythonPath = '/usr/sbin/python3', --function()
+    --     local path
+    --     for _, server in pairs(vim.lsp.buf_get_clients()) do
+    --         path = vim.tbl_get(server, 'config', 'settings', 'python', 'pythonPath')
+    --         if path then break end
+    --     end
+    --     path = vim.fn.input('Python path: ', path or '', 'file')
+    --     return path ~= '' and vim.fn.expand(path) or nil
+    -- end,
+    -- args = function()
+    --     local args = {}
+    --     local i = 1
+    --     while true do
+    --         local arg = vim.fn.input("Argument [" .. i .. "]: ")
+    --         if arg == '' then
+    --             break
+    --         end
+    --         args[i] = arg
+    --         i = i + 1
+    --     end
+    --     return args
+    -- end,
     justMyCode = function()
         return vim.fn.input('justMyCode? [y/n]: ') == 'y'
     end,
