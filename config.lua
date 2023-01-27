66d3c1ad9666a84aef4391205bc6d4d487cd5b4b
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-01-27 06:32:07 -0500

    q

--- config.lua
+++ config.lua
@@ -618,43 +618,15 @@ lvim.plugins = {
   },
   -- hignlight chunk signcolumn plug of nvim
   { "yaocccc/nvim-hlchunk" },
-  -- quickly interact with the repl inside neovim
   {
-    "hkupty/iron.nvim",
-    config = function ()
-      require('iron.core').setup{
-        config = {
-            -- Whether a repl should be discarded or not
-            scratch_repl = true,
-            -- Your repl definitions come here
-            repl_definition = {
-              sh = {
-                -- Can be a table or a function that
-                -- returns a table (see below)
-                command = {"zsh"}
-              }
-            },
-            -- How the repl window will be displayed
-            -- See below for more information
-            repl_open_cmd = require('iron.view').bottom(0.3),
-          },
-          -- If the highlight is on, you can change how it looks
-          -- For the available options, check nvim_set_hl
-          highlight = {
-            italic = true
-          },
-        }
-      end,
-    },
-    {
-      'sudormrfbin/cheatsheet.nvim',
+    'sudormrfbin/cheatsheet.nvim',
 
-      dependencies = {
-        {'nvim-telescope/telescope.nvim'},
-        {'nvim-lua/popup.nvim'},
-        {'nvim-lua/plenary.nvim'},
-      }
-    },
+    dependencies = {
+      {'nvim-telescope/telescope.nvim'},
+      {'nvim-lua/popup.nvim'},
+      {'nvim-lua/plenary.nvim'},
+    }
+  },
 }
 -- }}}1
 
